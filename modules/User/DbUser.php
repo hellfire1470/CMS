5514e491a6bad3858657f9f313df4ae251df4abc
hellfire1470 <9080146+hellfire1470@users.noreply.github.com>
2018-04-11 18:14:21 +0200

    added meta & reworked

--- modules/User/DbUser.php
+++ modules/User/DbUser.php
@@ -11,35 +11,45 @@
  *
  * @author Alexander
  */
-use K3ksPHP\Database\DbTable as DbT;
-use K3ksPHP\Database\DbField as DbF;
-use K3ksPHP\Database\DbKeyValue as DbKV;
-use K3ksPHP\Database\DbFieldType as DbFT;
-use K3ksPHP\Database\DbFieldAttribute as DbFA;
+use K3ksPHP\Database as Db;
 
 class DbUser {
 
-    private static $_db = null;
+    private static $_db          = null;
+    private static $_db_meta     = null;
     private static $_initialized = false;
 
-    private static function _Initialize() {
+    public static function Initialize() {
         if (!self::$_initialized) {
             self::$_initialized = true;
-            self::$_db = new DbT("users", [
-                new DbF('id', DbFT::INTEGER, 11, [DbFA::AUTO_INCREMENT, DbFA::PRIMARY_KEY]),
-                new DbF('firstname', DbFT::VARCHAR, 200),
-                new DbF('lastname', DbFT::VARCHAR, 200),
-                new DbF('password', DbFT::VARCHAR, 512),
-                new DbF('email', DbFT::VARCHAR, 200, [DbFA::UNIQUE, DbFA::NOT_NULL])
+            self::$_db          = new Db\Table("users", [
+                new Db\Field('id', Db\FieldType::INTEGER, 11, [Db\FieldAttribute::AUTO_INCREMENT, Db\FieldAttribute::PRIMARY_KEY]),
+                new Db\Field('firstname', Db\FieldType::VARCHAR, 200),
+                new Db\Field('lastname', Db\FieldType::VARCHAR, 200),
+                new Db\Field('password', Db\FieldType::VARCHAR, 512),
+                new Db\Field('email', Db\FieldType::VARCHAR, 200, [Db\FieldAttribute::UNIQUE, Db\FieldAttribute::NOT_NULL])
             ]);
-
-            self::$_db->Create();
+            self::$_db_meta     = new Db\Table("users_meta", [
+                new Db\Field('id', Db\FieldType::INTEGER, 11, [Db\FieldAttribute::AUTO_INCREMENT, Db\FieldAttribute::PRIMARY_KEY]),
+                new Db\Field('userid', Db\FieldType::INTEGER, 11, [Db\FieldAttribute::NOT_NULL]),
+                new Db\Field('metakey', Db\FieldType::VARCHAR, 255, [Db\FieldAttribute::NOT_NULL]),
+                new Db\Field('metavalue', Db\FieldType::TEXT),
+                new Db\Rule(Db\RuleType::UNIQUE, ['userid', 'metakey'])
+            ]);
         }
+    }
+
+    public static function GetUserTable() {
+        return self::$_db;
+    }
+
+    public static function GetUserMetaTable() {
+        return self::$_db_meta;
     }
 
     public static function Set($key_values) {
         if (!self::$_initialized) {
-            self::_Initialize();
+            self::Initialize();
         }
 
         return self::$_db->Set($key_values);
@@ -59,7 +69,7 @@ class DbUser {
 
     public static function GetByEmail($email) {
         if (!self::$_initialized) {
-            self::_Initialize();
+            self::Initialize();
         }
         $result = self::$_db->LoadAllByColumn('email', $email);
         return !empty($result) ? $result[0] : null;
